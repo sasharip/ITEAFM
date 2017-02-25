04164b1666f277ee51822f10462d621d37d06525
vattghern777 <vattghern777@gmail.com>
2017-02-26 01:37:54 +0200

    app rehaul missed classes

---
+++ app/src/main/java/com/vattghern/iteafm/Constants.java
@@ -0,0 +1,21 @@
+package com.vattghern.iteafm;
+
+/**
+ * Created by vattg on 16.02.2017.
+ */
+public interface Constants {
+
+  public static final int INTENT_COPY = 1;
+  public static final int INTENT_MOVE = 2;
+  public static final String DIRECTORY_COPY_TO = "212121";
+  public static final String PATH = "we34efsd";
+  public static final String CATEGORY = "Category";
+  public static final String PICTURES = "Pictures";
+  public static final String VIDEOS = "Videos";
+  public static final String MUSIC = "Music";
+  public static final String DOCUMENTS = "Documents";
+  public static final String FAVORITES = "Favorites";
+  public static final int INTENT_MAIN = 1212;
+}
+
+
